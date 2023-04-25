9becf1eab200d71dfebcfc9f81718988a138e268
domagoj.vukovic <off.vukovic@gmail.com>
2023-04-25 20:41:07 +0200

    feat: add lint

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+/** @type { import('@types/eslint').ESLint.ConfigData } */
+module.exports = {
+    overrides: [
+        {
+            extends: 'plugin:@graphql-eslint/schema-all',
+            files: ['./**/*.graphql'],
+            parserOptions: {
+                schema: './**/*.graphql',
+            },
+            rules: {
+                '@graphql-eslint/no-scalar-result-type-on-mutation': 'off',
+                '@graphql-eslint/require-description': 'off',
+                '@graphql-eslint/require-field-of-type-query-in-mutation-result': 'off',
+                '@graphql-eslint/input-name': 'off',
+                '@graphql-eslint/strict-id-in-types': [
+                    'error',
+                    {
+                        acceptedIdTypes: ['ID'],
+                        exceptions: {
+                            suffixes: [
+                                'Payload',
+                                'Pagination',
+                            ],
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            excludedFiles: [
+                './lib/**/*',
+            ],
+            extends: [require.resolve('@rimac-technology/style-guide/eslint/core')],
+            files: ['./**/*.{js,ts,mjs}'],
+            parser: '@typescript-eslint/parser',
+            parserOptions: {
+                project: './tsconfig.lint.json',
+            },
+        },
+    ],
+}
